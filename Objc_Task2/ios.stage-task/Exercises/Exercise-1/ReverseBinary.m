d9b382c428395ee2f8d88eeaba00614bfb28a417
Alisher <alishktl99@gmail.com>
2021-05-17 19:34:04 +0600

    done with first 3 tasks

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,13 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    UInt8 ans = 0;
+    for(NSInteger i = 0; i<8; i++){
+        if(n%2){
+            ans += pow(2,7-i);
+            printf("%hhu", ans);
+        }
+        n/=2;
+    }
+    return ans;
 }
--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,26 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
+    NSMutableString *ans = [[NSMutableString alloc] init];
+    if (numbersArray.count > 0) {
+        for (NSInteger i = 0; i < 4; i++) {
+            NSInteger num = 0;
+            if (i < numbersArray.count) {
+                num = [numbersArray[i] intValue];
+            }
+            if (num < 0){
+                return @"Negative numbers are not valid for input.";
+            }
+            else if (num > 255){
+                return @"The numbers in the input array can be in the range from 0 to 255.";
+            }
+            else{
+                [ans appendFormat:@"%ld.", num];
+            }
+        }
+        return [ans substringToIndex: [ans length] - 1];
+    }
+    return @"";
 }
 
 @end
